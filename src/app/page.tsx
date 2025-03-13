ac670719f6d9ec262a5c2cd7e204f279b3cab5c2
Nicolas Renard <nicolasrenard1999@gmail.com>
2025-03-13 19:16:42 -0300

    Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <>
+      <header>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/logo.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={200}
+          height={80}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </header>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <div className={styles.mainContentText}>
+            <h1>First Lesson to success</h1>
+            <br />
+            <p>
+              Let's go together until the success my friend the lesson teaches
+              you what's the power of a good approach.
+              <br />
+              <br />
+              <b>Lucas Wunder - Mentor & Especialista Tech</b>
+            </p>
+          </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <iframe
+            width="800"
+            height="500"
+            src="https://www.youtube.com/embed/hAygxfX4CWc?si=ie4N0cRjVSspN5oI"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay;picture-in-picture;"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </main>
+      </div>
+      <footer className={styles.footer}>MIT - LICENSE</footer>
+    </>
   );
 }
