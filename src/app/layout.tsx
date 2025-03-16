a95289a70d0c68a9f072b8141ec166cba6b598a1
Nicolas Renard <nicolasrenard1999@gmail.com>
2025-03-16 14:35:47 -0300

    Adding meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
+
 import "./globals.css";
 
 const geistSans = Inter({
@@ -19,6 +21,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script async={true}>
+          {`
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1169606664618718');
+  fbq('track', 'PageView');
+`}
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1169606664618718&ev=PageView&noscript=1"
+            />
+          </noscript>
+          {/* <!-- End Meta Pixel Code --> */}
+        </script>
+      </Head>
       <body className={`${geistSans.variable}`}>{children}</body>
     </html>
   );
